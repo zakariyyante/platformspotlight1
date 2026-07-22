bbc6800825e34f8b34d858f4a7ee0448d6cc9475
Zakariyya <zakariyya.z@antetechnologies.com>
2026-07-22 11:32:47 +0400

    isa

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -131,17 +131,17 @@ export const brands: Brand[] = [
   //   votes: 12450,
     
   // },
-  // {
-  //   id: "SpinFin",
-  //   name: "SpinFin",
-  //   logo: "/brands/SPINFIN.png",
-  //   rating: 9.8,
-  //   bonus: "350% jusqu’à 10000€ + 500FS",
-  //   url: "https://go.driveaffiliates.com/visit/?bta=35516&nci=5358&utm_campaign=SpinFin-SL2FR9&afp=",
-  //   isMobile: true,
-  //   votes: 12450,
+  {
+    id: "SpinFin",
+    name: "SpinFin",
+    logo: "/brands/SPINFIN.png",
+    rating: 9.8,
+    bonus: "350% jusqu’à 10000€ + 500FS",
+    url: "https://go.driveaffiliates.com/visit/?bta=35516&nci=5358&utm_campaign=SpinFin-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
     
-  // },
+  },
   
   // {
   //   id: "Spinkong",
