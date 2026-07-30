4c348a101721e87359699a197045705c506eb848
Zakariyya <zakariyya.z@antetechnologies.com>
2026-07-30 10:19:12 +0400

    isa

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -85,17 +85,7 @@ export const brands: Brand[] = [
   //   votes: 12450,
   //   
   // },
-  {
-    id: "SpinHub",
-    name: "SpinHub",
-    logo: "/brands/spinhub.png",
-    rating: 9.8,
-    bonus: "300% jusqu'à 1500€ + 100FS",
-    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Meilleure Offre", color: "#00f5ff" },
-  },
+ 
   {
     id: "SpinFormula",
     name: "SpinFormula",
@@ -107,7 +97,17 @@ export const brands: Brand[] = [
     votes: 12450,
     badge: { text: "Nouvelle Offre", color: "#a020f0" },
   },
- 
+  {
+    id: "SpinHub",
+    name: "SpinHub",
+    logo: "/brands/spinhub.png",
+    rating: 9.8,
+    bonus: "300% jusqu'à 1500€ + 100FS",
+    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  },
   //  {
   //   id: "bananzia",
   //   name: "Bananzia",
