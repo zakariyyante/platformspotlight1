06258b90a576a9879e0d468361e251fcbb9bec4c
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-01 00:06:29 +0400

    isa

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -65,17 +65,6 @@ export const brands: Brand[] = [
   //   
   // },
   {
-    id: "SpinHub",
-    name: "SpinHub",
-    logo: "/brands/spinhub.png",
-    rating: 9.8,
-    bonus: "300% jusqu'à 1500€ + 100FS",
-    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Meilleure Offre", color: "#00f5ff" },
-  },
-  {
     id: "WinKingdom",
     name: "WinKingdom",
     logo: "/brands/winkingdom.svg",
@@ -86,6 +75,18 @@ export const brands: Brand[] = [
     votes: 12450,
     badge: { text: "Retraits Rapides", color: "#22c55e" },
   },
+  {
+    id: "SpinHub",
+    name: "SpinHub",
+    logo: "/brands/spinhub.png",
+    rating: 9.8,
+    bonus: "300% jusqu'à 1500€ + 100FS",
+    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  },
+ 
   
   {
     id: "SpinFormula",
