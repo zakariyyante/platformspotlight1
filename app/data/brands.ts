2344554842bcc19f687405f2c9c68ed49e2bbbb3
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-05 11:18:51 +0400

    isa

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -31,17 +31,7 @@ export const brands: Brand[] = [
   //   
   // },
   
-  // {
-  //   id: "Betory",
-  //   name: "Betory",
-  //   logo: "/brands/betory.png",
-  //   rating: 9.8,
-  //   bonus: "300% jusqu'à 3250€",
-  //   url: "https://betorytrackers.com/d9c0p3tke?subid=BetNJet-SL2FR9&visit_id=",
-  //   isMobile: true,
-  //   votes: 12450,
-  //   
-  // },
+ 
  
   // {
   //   id: "Atefia",
@@ -82,17 +72,17 @@ export const brands: Brand[] = [
   
   
    
-  {
-    id: "SpinHub",
-    name: "SpinHub",
-    logo: "/brands/spinhub.png",
-    rating: 9.8,
-    bonus: "300% jusqu'à 1500€ + 100FS",
-    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Meilleure Offre", color: "#00f5ff" },
-  },
+  // {
+  //   id: "SpinHub",
+  //   name: "SpinHub",
+  //   logo: "/brands/spinhub.png",
+  //   rating: 9.8,
+  //   bonus: "300% jusqu'à 1500€ + 100FS",
+  //   url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+  //   isMobile: true,
+  //   votes: 12450,
+  //   badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  // },
   {
     id: "SpinFormula",
     name: "SpinFormula",
@@ -104,17 +94,17 @@ export const brands: Brand[] = [
     votes: 12450,
     badge: { text: "Nouvelle Offre", color: "#a020f0" },
   },
-  {
-    id: "Royalen",
-    name: "Royalen",
-    logo: "/brands/royalen.webp",
-    rating: 9.8,
-    bonus: "200% jusqu'à 2000€ + 200FS",
-    url: "https://royalmedialinks.com/j2qyjvhin?subid=Royalen-SL2FR9&visit_id=",
-    isMobile: true,
-    votes: 12450,
+  // {
+  //   id: "Royalen",
+  //   name: "Royalen",
+  //   logo: "/brands/royalen.webp",
+  //   rating: 9.8,
+  //   bonus: "200% jusqu'à 2000€ + 200FS",
+  //   url: "https://royalmedialinks.com/j2qyjvhin?subid=Royalen-SL2FR9&visit_id=",
+  //   isMobile: true,
+  //   votes: 12450,
     
-  },
+  // },
   
   // {
   //   id: "Spinlynx",
@@ -180,18 +170,28 @@ export const brands: Brand[] = [
   //   votes: 12450,
   //   
   // },
-  // {
-  //   id: "BetRiviera",
-  //   name: "BetRiviera",
-  //   logo: "/brands/BETRIVIERA.svg",
-  //   rating: 9.8,
-  //   bonus: "Jusqu’à 2000€ + 200FS",
-  //   url: "https://bestcpa.online/click?o=567&a=36&sub_id1=Betriviera-SL2FR9&aff_click_id=",
-  //   isMobile: true,
-  //   votes: 12450,
+  {
+    id: "BetRiviera",
+    name: "BetRiviera",
+    logo: "/brands/BETRIVIERA.svg",
+    rating: 9.8,
+    bonus: "Jusqu’à 2000€ + 200FS",
+    url: "https://bestcpa.online/click?o=567&a=36&sub_id1=Betriviera-SL2FR9&aff_click_id=",
+    isMobile: true,
+    votes: 12450,
     
-  // },
- 
+  },
+  {
+    id: "Betory",
+    name: "Betory",
+    logo: "/brands/betory.png",
+    rating: 9.8,
+    bonus: "300% jusqu'à 3250€",
+    url: "https://betorytrackers.com/d9c0p3tke?subid=BetNJet-SL2FR9&visit_id=",
+    isMobile: true,
+    votes: 12450,
+    
+  },
   // {
   //   id: "RxBet",
   //   name: "RxBet",
