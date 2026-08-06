6b92abcf8f988ed09b9e47f46d6b15e539db59fc
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-07 00:23:00 +0400

    spooderman

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -44,17 +44,7 @@ export const brands: Brand[] = [
   //   votes: 12450,
   //   
   // },
-  //  {
-  //   id: "Royalen",
-  //   name: "Royalen",
-  //   logo: "/brands/royalen.webp",
-  //   rating: 9.8,
-  //   bonus: "200% jusqu'à 2000€ + 200FS",
-  //   url: "https://royalmedialinks.com/j2qyjvhin?subid=Royalen-SL2FR9&visit_id=",
-  //   isMobile: true,
-  //   votes: 12450,
-  //   
-  // },
+  
   {
     id: "BetRiviera",
     name: "BetRiviera",
@@ -89,6 +79,17 @@ export const brands: Brand[] = [
     isMobile: true,
     votes: 12450,
     badge: { text: "Retraits Rapides", color: "#22c55e" },
+  },
+   {
+    id: "Royalen",
+    name: "Royalen",
+    logo: "/brands/royalen.webp",
+    rating: 9.8,
+    bonus: "200% jusqu'à 2000€ + 200FS",
+    url: "https://royalmedialinks.com/j2qyjvhin?subid=Royalen-SL2FR9&visit_id=",
+    isMobile: true,
+    votes: 12450,
+    
   },
   // {
   //   id: "WoolBet",
