a7fc1d6ec1b8c63f0ccf51dbd587cf7ce8f882a5
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-08 01:05:02 +0400

    first

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -58,7 +58,17 @@ export const brands: Brand[] = [
     votes: 12450,
     badge: { text: "Nouvelle Offre", color: "#a020f0" },
   },
-
+  {
+    id: "Zombillion",
+    name: "Zombillion",
+    logo: "/brands/zombillion.svg",
+    rating: 9.8,
+    bonus: "250% jusqu’à 4000€ + 200FS",
+    url: "https://direct.club.partners/click?pid=19&offer_id=59&l=1779872569&sub2=Zombillion-SL2FR9&sub1=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Retraits Rapides", color: "#22c55e" },
+  },
   // {
   //   id: "WinKingdom",
   //   name: "WinKingdom",
@@ -129,17 +139,17 @@ export const brands: Brand[] = [
   
   
    
-  {
-    id: "SpinHub",
-    name: "SpinHub",
-    logo: "/brands/spinhub.png",
-    rating: 9.8,
-    bonus: "300% jusqu'à 1500€ + 100FS",
-    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Retraits Rapides", color: "#22c55e" },
-  },
+  // {
+  //   id: "SpinHub",
+  //   name: "SpinHub",
+  //   logo: "/brands/spinhub.png",
+  //   rating: 9.8,
+  //   bonus: "300% jusqu'à 1500€ + 100FS",
+  //   url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+  //   isMobile: true,
+  //   votes: 12450,
+    
+  // },
   
   // {
   //   id: "Royalen",
@@ -287,17 +297,7 @@ export const brands: Brand[] = [
   //   votes: 12450,
     
   // },
-  // {
-  //   id: "Zombillion",
-  //   name: "Zombillion",
-  //   logo: "/brands/zombillion.svg",
-  //   rating: 9.8,
-  //   bonus: "250% jusqu’à 4000€ + 200FS",
-  //   url: "https://direct.club.partners/click?pid=19&offer_id=59&l=1779872569&sub2=Zombillion-SL2FR9&sub1=",
-  //   isMobile: true,
-  //   votes: 12450,
-    
-  // },
+  
   // {
   //   id: "BetNJet",
   //   name: "BetNJet",
