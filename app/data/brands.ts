a0487b3e4e87482a2676dc84ce0c826313e30e19
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-10 18:15:22 +0400

    first

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -58,17 +58,17 @@ export const brands: Brand[] = [
     votes: 12450,
     badge: { text: "Nouvelle Offre", color: "#a020f0" },
   },
-   {
-    id: "Aerobet",
-    name: "Aerobet",
-    logo: "/brands/AEROBET.webp",
-    rating: 9.8,
-    bonus: "Jusqu’à 600% + 450FS",
-    url: "https://aerobetalive.com/tawbnds9h?subid=Aerobet-SL2FR9&visit_id=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Retraits Rapides", color: "#22c55e" },
-  },
+  //  {
+  //   id: "Aerobet",
+  //   name: "Aerobet",
+  //   logo: "/brands/AEROBET.webp",
+  //   rating: 9.8,
+  //   bonus: "Jusqu’à 600% + 450FS",
+  //   url: "https://aerobetalive.com/tawbnds9h?subid=Aerobet-SL2FR9&visit_id=",
+  //   isMobile: true,
+  //   votes: 12450,
+  //   
+  // },
 
      {
     id: "BravoPlay",
@@ -82,6 +82,17 @@ export const brands: Brand[] = [
     badge: { text: "Meilleure Offre", color: "#00f5ff" },
   }
   ,
+   {
+    id: "SpinHub",
+    name: "SpinHub",
+    logo: "/brands/spinhub.png",
+    rating: 9.8,
+    bonus: "300% jusqu'à 1500€ + 100FS",
+    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Retraits Rapides", color: "#22c55e" },
+  },
   // {
   //   id: "Zombillion",
   //   name: "Zombillion",
@@ -188,17 +199,7 @@ export const brands: Brand[] = [
   
   
    
-  // {
-  //   id: "SpinHub",
-  //   name: "SpinHub",
-  //   logo: "/brands/spinhub.png",
-  //   rating: 9.8,
-  //   bonus: "300% jusqu'à 1500€ + 100FS",
-  //   url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-  //   isMobile: true,
-  //   votes: 12450,
-    
-  // },
+ 
   
   // {
   //   id: "Royalen",
