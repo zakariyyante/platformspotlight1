7a966883bbaad58661bafd0913b7c0cc8a6d3ead
Zakariyya <zakariyya.z@antetechnologies.com>
2026-08-11 12:33:44 +0400

    first

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -47,18 +47,18 @@ export const brands: Brand[] = [
   //   
   
   // },
-  {
-    id: "SpinHub",
-    name: "SpinHub",
-    logo: "/brands/spinhub.png",
-    rating: 9.8,
-    bonus: "300% jusqu'à 1500€ + 100FS",
-    url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  // {
+  //   id: "SpinHub",
+  //   name: "SpinHub",
+  //   logo: "/brands/spinhub.png",
+  //   rating: 9.8,
+  //   bonus: "300% jusqu'à 1500€ + 100FS",
+  //   url: "https://on.spinhub100.com/promoRedirect?key=ej0xMzUyNTU2NCZsPTEzNTI1MDA3JnA9MzYzMA%253D%253D&var4=fr&var2=SpinHub-SL2FR9&var1=",
+  //   isMobile: true,
+  //   votes: 12450,
     
-  },
+    
+  // },
   {
     id: "SpinFormula",
     name: "SpinFormula",
@@ -120,17 +120,17 @@ export const brands: Brand[] = [
   //   votes: 12450,
   //   
   // },
-  //  {
-  //   id: "RunaCasino",
-  //   name: "RunaCasino",
-  //   logo: "/brands/runa.svg",
-  //   rating: 9.8,
-  //   bonus: "Jusqu’à 2500€ + 600FS",
-  //   url: "https://bestcpa.online/click?o=581&a=36&sub_id1=RunaCasino-SL2FR9&aff_click_id=",
-  //   isMobile: true,
-  //   votes: 12450,
-  //   
-  // },
+   {
+    id: "RunaCasino",
+    name: "RunaCasino",
+    logo: "/brands/runa.svg",
+    rating: 9.8,
+    bonus: "Jusqu’à 2500€ + 600FS",
+    url: "https://bestcpa.online/click?o=581&a=36&sub_id1=RunaCasino-SL2FR9&aff_click_id=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  },
   //  {
   //   id: "Westace",
   //   name: "Westace",
