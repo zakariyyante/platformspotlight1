c2f28baf0530ee126cfdb98a273aad243e482631
U N I S E T <user@MacBook-Air-3.local>
2026-08-14 10:28:05 +0400

    Update brands

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -60,6 +60,17 @@ export const brands: Brand[] = [
     
   // },
   {
+    id: "WinoCasino",
+    name: "WinoCasino",
+    logo: "/brands/wino.svg",
+    rating: 9.7,
+    bonus: "600% jusqu’à 10000€",
+    url: "https://go.afftrackio.com/visit/?bta=35487&nci=5357&utm_campaign=WinoCasino-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Nouvelle Offre", color: "#a020f0" },
+  },
+  {
     id: "SpinFormula",
     name: "SpinFormula",
     logo: "/brands/Spinformula.svg",
@@ -83,19 +94,6 @@ export const brands: Brand[] = [
     badge: { text: "Retraits Rapides", color: "#22c55e" },
 
   },
-
-  {
-    id: "WinoCasino",
-    name: "WinoCasino",
-    logo: "/brands/wino.svg",
-    rating: 9.7,
-    bonus: "600% jusqu’à 10000€",
-    url: "https://go.afftrackio.com/visit/?bta=35487&nci=5357&utm_campaign=WinoCasino-SL2FR9&afp=",
-    isMobile: true,
-    votes: 12450,
-    badge: { text: "Nouvelle Offre", color: "#a020f0" },
-  },
-  
   {
     id: "BravoPlay",
     name: "BravoPlay",
