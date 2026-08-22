261b8509c121c2cb3188cb2c42f88e254c432e43
U N I S E T <user@MacBook-Air-3.local>
2026-08-22 16:43:20 +0400

    fix error

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -58,6 +58,18 @@ export const brands: Brand[] = [
   //   votes: 12450,
   // },
   {
+    id: "SpinFormula",
+    name: "SpinFormula",
+    logo: "/brands/Spinformula.svg",
+    rating: 9.8,
+    bonus: "550% jusqu’à 15000€ + 550FS",
+    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+  },
+
+  {
     id: "WinoCasino",
     name: "WinoCasino",
     logo: "/brands/wino.svg",
@@ -66,7 +78,7 @@ export const brands: Brand[] = [
     url: "https://go.afftrackio.com/visit/?bta=35487&nci=5357&utm_campaign=WinoCasino-SL2FR9&afp=",
     isMobile: true,
     votes: 12450,
-    badge: { text: "Meilleure Offre", color: "#00f5ff" },
+    
     
   },
 
