751aacdb534158772dcf25c4fa7e84beaa1d64d6
U N I S E T <user@MacBook-Air-3.local>
2026-08-26 20:22:28 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -69,6 +69,17 @@ export const brands: Brand[] = [
     // badge: { text: "Retraits Rapides", color: "#22c55e" },
     badge: { text: "Meilleur Offre", color: "#00F0FF" },
   },
+  {
+    id: "SpinFormula",
+    name: "SpinFormula",
+    logo: "/brands/Spinformula.svg",
+    rating: 9.8,
+    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
+    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
+    badge: { text: "Retraits Rapides", color: "#22c55e" },
+  },
 
   {
     id: "WinKingdom",
@@ -82,19 +93,7 @@ export const brands: Brand[] = [
     
   },
 
-  {
-    id: "SpinFormula",
-    name: "SpinFormula",
-    logo: "/brands/Spinformula.svg",
-    rating: 9.8,
-    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
-    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
-    isMobile: true,
-    votes: 12450,
-    // badge: { text: "Meilleur Offre", color: "#00F0FF" },
-  },
   
-
 
   {
     id: "BetPortal",
