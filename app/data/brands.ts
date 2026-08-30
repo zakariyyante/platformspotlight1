c286de14cd9cec1c58bd979b669c4557bd66b387
U N I S E T <user@MacBook-Air-3.local>
2026-08-30 11:23:14 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -84,18 +84,36 @@ export const brands: Brand[] = [
     
     
   },
+
+
   {
-    id: "Chanze",
-    name: "Chanze",
-    logo: "/brands/chanze_dark.svg",
-    rating: 9.9,
-    bonus: "Pack de Bienvenue 650% jusqu’à 6500€",
-    url: "https://go.affililinks.com/visit/?bta=35328&nci=5352&utm_campaign=Chanze-SL2FR9&afp=",
+    id: "GoldenPlay",
+    name: "GoldenPlay",
+    logo: "/brands/goldenplay_dark.svg",
+    rating: 9.8,
+    bonus: "Pack de Bienvenue 100% jusqu’à 555€ + 100 Tours Gratuits",
+    url: "https://record.gplaynetopartners.com/_vly5BcOM0H2Capx6qLanuGNd7ZgqdRLk/9/?pg=3&subid=GoldenPlay-SL2FR9&cid=",
     isMobile: true,
     votes: 12450,
+    // badge: { text: "Nouvelle Offre", color: "#a020f0" },
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
     // badge: { text: "Meilleur Offre", color: "#00F0FF" },
-    
+ 
   },
+
+
+    // {
+  //   id: "Chanze",
+  //   name: "Chanze",
+  //   logo: "/brands/chanze_dark.svg",
+  //   rating: 9.9,
+  //   bonus: "Pack de Bienvenue 650% jusqu’à 6500€",
+  //   url: "https://go.affililinks.com/visit/?bta=35328&nci=5352&utm_campaign=Chanze-SL2FR9&afp=",
+  //   isMobile: true,
+  //   votes: 12450,
+  //   // badge: { text: "Meilleur Offre", color: "#00F0FF" },
+    
+  // },
 
 
 
@@ -271,19 +289,7 @@ export const brands: Brand[] = [
 
 
 
-  // {
-  //   id: "GoldenPlay",
-  //   name: "GoldenPlay",
-  //   logo: "/brands/goldenplay_dark.svg",
-  //   rating: 9.8,
-  //   bonus: "Pack de Bienvenue 100% jusqu’à 555€ + 100 Tours Gratuits",
-  //   url: "https://record.gplaynetopartners.com/_vly5BcOM0H2Capx6qLanuGNd7ZgqdRLk/9/?pg=3&subid=GoldenPlay-SL2FR9&cid=",
-  //   isMobile: true,
-  //   votes: 12450,
-  //   // badge: { text: "Nouvelle Offre", color: "#a020f0" },
-  //   // badge: { text: "Retraits Rapides", color: "#22c55e" },
-  //   // badge: { text: "Meilleur Offre", color: "#00F0FF" },
-  // },
+
 
  
   
