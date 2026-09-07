0c5a0795ef918d5ec1a7a2d7538d055cfb4f19ca
U N I S E T <user@MacBook-Air-3.local>
2026-09-07 18:09:27 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -513,7 +513,7 @@ export const brands: Brand[] = [
     name: "BravoPlay",
     logo: "/brands/bravoplay.svg",
     rating: 9.8,
-    bonus: "Jusqu'à 5000€ + 150FS",
+    bonus: "Pack de Bienvenue Jusqu'à 5000€ + 150 Tours Gratuits",
     url: "https://trk.arztrc.site/click?o=1&a=82&c=314&link_id=39&sub_id1=BravoPlay-SL2FR9&aff_click_id=",
     isMobile: true,
     votes: 12450,
