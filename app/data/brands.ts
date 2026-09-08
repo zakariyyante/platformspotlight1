e74007ea3e7d4da590e085c66d64da194255e1d5
U N I S E T <user@MacBook-Air-3.local>
2026-09-08 14:48:33 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -173,7 +173,7 @@ export const brands: Brand[] = [
     isMobile: true,
     votes: 12450,
     
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    badge: { text: "Nouvelle Offre", color: "#a020f0" },
     // badge: { text: "Retraits Rapides", color: "#22c55e" },
     
   },
