6d2cfebed9fe69096609d5dcd13f9fe48df96009
U N I S E T <user@MacBook-Air-3.local>
2026-09-10 21:19:59 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -75,19 +75,7 @@ export const brands: Brand[] = [
     
   // },
 
-  {
-    id: "LuckyGem",
-    name: "LuckyGem",
-    logo: "/brands/luckygem_dark.svg",
-    rating: 9.9,
-    bonus: "Pack de Bienvenue 700% + 700 Tours Gratuits",
-    url: "https://bestcpa.online/click?o=440&a=36&link_id=829&sub_id1=LuckyGem-SL2FR9&aff_click_id=",
-    isMobile: true,
-    votes: 12450,
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
-    badge: { text: "Retraits Rapides", color: "#22c55e" },
 
-  },
 
   {
     id: "TowerCasino",
@@ -99,7 +87,7 @@ export const brands: Brand[] = [
     isMobile: true,
     votes: 12450,
     // badge: { text: "Nouvelle Offre", color: "#a020f0" },
-    
+    badge: { text: "Retraits Rapides", color: "#22c55e" },
     
   },
 
@@ -118,6 +106,21 @@ export const brands: Brand[] = [
     
   },
   
+  {
+    id: "LuckyGem",
+    name: "LuckyGem",
+    logo: "/brands/luckygem_dark.svg",
+    rating: 9.9,
+    bonus: "Pack de Bienvenue 700% + 700 Tours Gratuits",
+    url: "https://bestcpa.online/click?o=440&a=36&link_id=829&sub_id1=LuckyGem-SL2FR9&aff_click_id=",
+    isMobile: true,
+    votes: 12450,
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+
+  },
+
+
   {
     id: "Wildies",
     name: "Wildies",
