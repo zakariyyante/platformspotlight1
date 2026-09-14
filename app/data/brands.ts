364e9869918720a18b9343373c8a72dfb4dd0503
U N I S E T <user@MacBook-Air-3.local>
2026-09-14 13:03:15 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -94,6 +94,19 @@ export const brands: Brand[] = [
 
 
   {
+    id: "SpinFormula",
+    name: "SpinFormula",
+    logo: "/brands/Spinformula.svg",
+    rating: 9.8,
+    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
+    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    
+  },
+
+  {
     id: "LuckyGem",
     name: "LuckyGem",
     logo: "/brands/luckygem_dark.svg",
@@ -126,18 +139,7 @@ export const brands: Brand[] = [
 
 
 
-  {
-    id: "SpinFormula",
-    name: "SpinFormula",
-    logo: "/brands/Spinformula.svg",
-    rating: 9.8,
-    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
-    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
-    isMobile: true,
-    votes: 12450,
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
-    
-  },
+
 
 
 
