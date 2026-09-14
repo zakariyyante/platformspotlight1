4d08406e01d62311558b13e69953d586451dd177
U N I S E T <user@MacBook-Air-3.local>
2026-09-14 13:02:53 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -62,20 +62,20 @@ export const brands: Brand[] = [
 
 
 
-  {
-    id: "Wildies",
-    name: "Wildies",
-    logo: "/brands/wildies.svg",
-    rating: 9.8,
-    bonus: "Pack de Bienvenue 400% jusqu'à 3500€ + 150 Tours Gratuits",
-    url: "https://bestcpa.online/click?o=556&a=36&link_id=1021&sub_id1=Wildies-SL2FR9&aff_click_id=",
-    isMobile: true,
-    votes: 12450,
+  // {
+  //   id: "Wildies",
+  //   name: "Wildies",
+  //   logo: "/brands/wildies.svg",
+  //   rating: 9.8,
+  //   bonus: "Pack de Bienvenue 400% jusqu'à 3500€ + 150 Tours Gratuits",
+  //   url: "https://bestcpa.online/click?o=556&a=36&link_id=1021&sub_id1=Wildies-SL2FR9&aff_click_id=",
+  //   isMobile: true,
+  //   votes: 12450,
     
-    // badge: { text: "Nouvelle Offre", color: "#a020f0" },
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+  //   // badge: { text: "Nouvelle Offre", color: "#a020f0" },
+  //   // badge: { text: "Retraits Rapides", color: "#22c55e" },
     
-  },
+  // },
 
   {
     id: "WinPlace",
@@ -108,18 +108,18 @@ export const brands: Brand[] = [
   },
 
 
-  // {
-  //   id: "WinKingdom",
-  //   name: "WinKingdom",
-  //   logo: "/brands/winkingdom.svg",
-  //   rating: 9.8,
-  //   bonus: "Pack de Bienvenue Jusqu’à 2000€ + 100 Tours Gratuits",
-  //   url: "https://winkingdom.media/n11npxcmmu?subid=Winkingdom-SL2FR9&visit_id=",
-  //   isMobile: true,
-  //   votes: 12450,
+  {
+    id: "WinKingdom",
+    name: "WinKingdom",
+    logo: "/brands/winkingdom.svg",
+    rating: 9.8,
+    bonus: "Pack de Bienvenue Jusqu’à 2000€ + 100 Tours Gratuits",
+    url: "https://winkingdom.media/n11npxcmmu?subid=Winkingdom-SL2FR9&visit_id=",
+    isMobile: true,
+    votes: 12450,
     
 
-  // },
+  },
 
 
 
