dfa95f2ce3ecc88cb0da744d297f656c0f03a0da
U N I S E T <user@MacBook-Air-3.local>
2026-09-18 20:34:06 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -117,6 +117,21 @@ export const brands: Brand[] = [
   },
 
   {
+    id: "LuckyGem",
+    name: "LuckyGem",
+    logo: "/brands/luckygem_dark.svg",
+    rating: 9.9,
+    bonus: "Pack de Bienvenue 700% + 700 Tours Gratuits",
+    url: "https://bestcpa.online/click?o=440&a=36&link_id=829&sub_id1=LuckyGem-SL2FR9&aff_click_id=",
+    isMobile: true,
+    votes: 12450,
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    
+  },
+
+
+  {
     id: "RollySpin",
     name: "RollySpin",
     logo: "/brands/rollyspin.svg",
@@ -148,19 +163,7 @@ export const brands: Brand[] = [
     
   },
 
-  {
-    id: "LuckyGem",
-    name: "LuckyGem",
-    logo: "/brands/luckygem_dark.svg",
-    rating: 9.9,
-    bonus: "Pack de Bienvenue 700% + 700 Tours Gratuits",
-    url: "https://bestcpa.online/click?o=440&a=36&link_id=829&sub_id1=LuckyGem-SL2FR9&aff_click_id=",
-    isMobile: true,
-    votes: 12450,
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
-    
-  },
+
   
   // {
   //   id: "GambleGorilla",
