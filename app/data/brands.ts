c33eec1956ea8041a721801704fff2628d32f2cc
U N I S E T <user@MacBook-Air-3.local>
2026-09-18 13:36:14 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -105,6 +105,21 @@ export const brands: Brand[] = [
   },
 
   {
+    id: "RollySpin",
+    name: "RollySpin",
+    logo: "/brands/rollyspin.svg",
+    rating: 9.9,
+    bonus: "Pack de Bienvenue 425% jusqu'à 5500€ + 525 Tours Gratuits",
+    url: "https://rollyspin19.net/dtack2gez?subid=RollySpin-SL2FR9&visit_id=",
+    isMobile: true,
+    votes: 12450,
+    // badge: { text: "Nouvelle Offre", color: "#a020f0" },
+    
+    
+  },
+
+
+  {
     id: "SpinFormula",
     name: "SpinFormula",
     logo: "/brands/Spinformula.svg",
