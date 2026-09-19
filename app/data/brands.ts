da51fa92746725770fbb9b3db3d178e6127d6d19
U N I S E T <user@MacBook-Air-3.local>
2026-09-19 23:12:51 +0400

    your message

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -104,6 +104,20 @@ export const brands: Brand[] = [
   // },
 
   {
+    id: "SpinFormula",
+    name: "SpinFormula",
+    logo: "/brands/Spinformula.svg",
+    rating: 9.8,
+    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
+    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
+    isMobile: true,
+    votes: 12450,
+    // badge: { text: "Retraits Rapides", color: "#22c55e" },
+    
+  },
+
+
+  {
     id: "GambleGorilla",
     name: "GambleGorilla",
     logo: "/brands/gamblegorilla.svg",
@@ -136,18 +150,7 @@ export const brands: Brand[] = [
 
 
 
-  {
-    id: "SpinFormula",
-    name: "SpinFormula",
-    logo: "/brands/Spinformula.svg",
-    rating: 9.8,
-    bonus: "Pack de Bienvenue 550% jusqu’à 15000€ + 550 Tours Gratuits",
-    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR9&afp=",
-    isMobile: true,
-    votes: 12450,
-    // badge: { text: "Retraits Rapides", color: "#22c55e" },
-    
-  },
+
 
 
   {
